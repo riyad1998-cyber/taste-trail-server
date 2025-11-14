ad0648548e9b4ba8dd8ff0c7e39cf1faa90ac1ab
riyad1998-cyber <jobiyedriyad@gmail.com>
2025-11-14 14:48:43 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+const express = require('express')
+const cors = require('cors')
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express()
+const port = 3000;
+
+app.use(cors())
+app.use(express.json())
+
+
+
+
+const uri = "mongodb+srv://taste-Trail:4AbwBtRoWGdkBKTl@riyad.wtxen74.mongodb.net/?appName=riyad";
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    await client.connect();
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
